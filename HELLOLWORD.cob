cba844763a00d3f80a566972c5001ff58dc39c28
igorrm10 <igorrodriguesmachado23@gmail.com>
2025-08-07 21:08:53 -0300

    Add initial COBOL program to display greetings
    - Created HELLOLWORD.cob with a simple program that outputs "Hello world" and "Deu certo".

---
+++ HELLOLWORD.cob
@@ -0,0 +1,6 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+       PROCEDURE DIVISION.
+              DISPLAY "Hello world".
+              DISPLAY "Deu certo".
+                STOP RUN.
